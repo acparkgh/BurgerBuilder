c50987516faeda706b628b48019a5bcd6fbbeb1b
acparkgh <acpark123@gmail.com>
2021-02-02 15:11:18 -0500

    enabled More button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -9,13 +9,17 @@ const controls = [
 ];
 
 const buildControls = (props) => {
-  <div className={classes.BuildControls}>
-    {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label} /> 
-     ))
-    }
-
-  </div>
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+        <BuildControl key={ctrl.label} 
+                      label={ctrl.label}
+                      added={() => props.ingredientAdded(ctrl.type)} 
+        /> 
+        ))
+      }
+    </div>
+  )
 };
 
 export default buildControls;
