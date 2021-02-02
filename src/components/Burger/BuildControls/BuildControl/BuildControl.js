3541f12ed75f913fbe4c68c5861025575b5d5cc3
acparkgh <acpark123@gmail.com>
2021-02-02 16:11:33 -0500

    disabled Less button if count is 0 or less

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,6 +6,7 @@ const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}
+            disabled={props.disabled}
             onClick={props.removed} >Less</button>
     <button className={classes.More} 
             onClick ={props.added} >More</button>
