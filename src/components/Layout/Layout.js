58f1990e422092c74885d22c06acbd04516501c4
acparkgh <acpark123@gmail.com>
2021-02-05 16:34:25 -0500

    added Backdrop to Sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,14 +4,31 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content} >
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends React.Component {
 
-export default layout;
+  state = {
+    showSideDrawer: true,
+  }
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  render() {
+    return (
+    
+      <Aux>
+        <Toolbar />
+        <SideDrawer open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerCloseHandler} 
+        />
+        <main className={classes.Content} >
+          {this.props.children}
+        </main>
+      </Aux>
+ 
+   )
+  }
+};
+
+export default Layout;
