fb1253843e78a8547cf2d428319d9a2a8606a293
acparkgh <acpark123@gmail.com>
2021-02-25 18:27:22 -0500

    fetch orders from Firebase /Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
 
 class Orders extends Component {
+
+  state = {
+    orders: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(res => {
+        // console.log(res.data);
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          })
+        }
+        this.setState({ 
+          loading: false,
+          orders: fetchedOrders,
+         });
+      })
+      .catch(err => {
+        this.setState({
+          loading: false
+        });
+      })
+  }
 
   render() {
     return (
